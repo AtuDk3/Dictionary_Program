aab2c04d080f11c91acc0af5c623744e2a0f33fa
ATU <thanhtudzx@gmail.com>
2023-10-13 14:15:59 +0700

    first commit

---
+++ src/common/Library.java
@@ -0,0 +1,53 @@
+package common;
+
+import java.util.Scanner;
+
+public class Library {
+    Scanner sc = new Scanner(System.in, "UTF-8");
+   
+    public int inputIntLimit(int min, int max) {
+        while (true) {
+            try {
+                int result = Integer.parseInt(sc.nextLine().trim());
+                if (result < min || result > max) {
+                    throw new NumberFormatException();
+                }
+                return result;
+            } catch (NumberFormatException e) {
+                System.err.println("Please input number in rage [" + min + ", " + max + "]");
+                System.out.print("Enter again: ");
+            }
+        }
+    }
+    
+    public String inputString() {
+        while (true) {
+            String result = sc.nextLine().trim();
+            if (result.isEmpty()) {
+                System.err.println("Not empty!");
+                System.out.print("Enter again: ");
+            } else {
+                return result;
+            }
+        }
+    }
+    
+    public boolean checkInputYN() {
+        System.out.print("Do you want to replace(Y/N)? ");
+        //loop until user input correct
+        while (true) {
+            String result = inputString();
+            //return true if user input y/Y
+            if (result.equalsIgnoreCase("Y")) {
+                return true;
+            }
+            //return false if user input n/N
+            if (result.equalsIgnoreCase("N")) {
+                return false;
+            }
+            System.err.println("Please input y/Y or n/N.");
+            System.out.print("Enter again: ");
+        }
+    }
+
+}
